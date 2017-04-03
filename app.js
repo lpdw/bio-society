94f33cec5786f622bc8cad2cffb27d0d347e48e8
Joshua <vegas.joshua@gmail.com>
2017-04-04 01:11:14 +0200

    Install session and sequelize dependency.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+var sess = {
+  secret: 'bio-society-api',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+app.use(session(sess));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
