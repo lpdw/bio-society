b7266c27f9302d7e4e13a63b5d22d4b4cf515b77
AlexandreLacr <alexandrelac248@gmail.com>
2017-04-04 10:00:58 +0200

    Add database

---
+++ database/users.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('Users', {
+        username: {
+           type: DataTypes.STRING,
+           validate: {notEmpty: {msg: "-> Missing username"}}
+        },
+        password: {
+           type: DataTypes.STRING,
+           validate: {notEmpty: {msg: "-> Missing password"}}
+        },
+        displayName: {
+           type: DataTypes.STRING,
+           validate: {notEmpty: {msg: "-> Missing displayName"}}
+		} 
+	});
+};
